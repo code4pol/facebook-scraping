c2c37503408b578785855de2e205e1a5c785c6f6
Alexandre Gomes <alegomes@gmail.com>
2017-04-06 10:26:04 -0300

    Remoção do singleton

--- facebook/facebook.py
+++ facebook/facebook.py
@@ -1,58 +1,108 @@
 from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 
+# class Facebook:
+#
+#     instance = None
+#
+#     def __init__(self, username, password):
+#         print('Facebook.__init__')
+#         if not Facebook.instance:
+#             Facebook.instance = Facebook.__FacebookLoginPage(username, password)
+#
+#     # Delegator
+#     def __getattr__(self, name):
+#         return getattr(self.instance, name)
+#
+#     def __del__(self):
+#         # self.instance._driver.close()
+#         self.instance._driver.quit()
+#         print('FacebookLoginPage destroyed')
+#
+#     # Singleton
+#     # http://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
+#     class __FacebookLoginPage:
+#
+#         _login_url = "https://www.facebook.com/"
+#         logged = False
+#
+#         def __init__(self, username, password):
+#             print('__FacebookLoginPage.__init__')
+#             self._driver = webdriver.Chrome('/Users/alegomes/code/facebook-scraping/drivers/chromedriver')
+#             # self._driver = webdriver.Firefox(executable_path='/Users/alegomes/code/facebook-scraping/drivers/geckodriver')
+#             self._driver.get(self._login_url)
+#             assert "Facebook - Log In or Sign Up" in self._driver.title
+#             self._email_field = self._driver.find_element_by_id('email')
+#             self._pass_field = self._driver.find_element_by_id('pass')
+#             self._login_button = self._driver.find_element_by_id('u_0_p')
+#
+#             self._login(username,password)
+#
+#         def __del__(self):
+#             # self._driver.close()
+#             self._driver.quit()
+#             print('__FacebookLoginPage destroyed')
+#
+#         def _login(self, user, password):
+#             if not self.logged:
+#                 self._email_field.send_keys(user)
+#                 self._pass_field.send_keys(password)
+#                 # self._login_button.click()
+#                 self._pass_field.submit()
+#                 self.logged = True
+#
+#
+#         def open_fanpage(self, url):
+#             self._driver.get(url)
+#             return self._driver
+#
+#         def close(self):
+#             self._driver.quit()
+
+
+
 class Facebook:
 
-    instance = None
+    _login_url = "https://www.facebook.com/"
+    logged = False
 
     def __init__(self, username, password):
-        if not Facebook.instance:
-            Facebook.instance = Facebook.__FacebookLoginPage(username, password)
+        print('Facebook.__init__')
+        self._driver = webdriver.Chrome('/Users/alegomes/code/facebook-scraping/drivers/chromedriver')
+        # self._driver = webdriver.Firefox(executable_path='/Users/alegomes/code/facebook-scraping/drivers/geckodriver')
+        self._driver.get(self._login_url)
+        assert "Facebook - Log In or Sign Up" in self._driver.title
+        self._email_field = self._driver.find_element_by_id('email')
+        self._pass_field = self._driver.find_element_by_id('pass')
+        self._login_button = self._driver.find_element_by_id('u_0_p')
 
-    # Delegator
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
+        self._login(username,password)
 
     def __del__(self):
-        self.instance._driver.close()
-        self.instance._driver.quit()
-        print('FacebookLoginPage destroyed')
+        # self._driver.close()
+        self._driver.quit()
+        print('Facebook destroyed')
 
-    # Singleton
-    # http://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
-    class __FacebookLoginPage:
-
-        _login_url = "https://www.facebook.com/"
-        logged = False
-
-        def __init__(self, username, password):
-            self._driver = webdriver.Chrome('/Users/alegomes/code/facebook-scraping/drivers/chromedriver')
-            # self._driver = webdriver.Firefox(executable_path='/Users/alegomes/code/facebook-scraping/drivers/geckodriver')
-            self._driver.get(self._login_url)
-            assert "Facebook - Log In or Sign Up" in self._driver.title
-            self._email_field = self._driver.find_element_by_id('email')
-            self._pass_field = self._driver.find_element_by_id('pass')
-            self._login_button = self._driver.find_element_by_id('u_0_p')
-
-            self._login(username,password)
-
-        def __del__(self):
-            self._driver.close()
-            self._driver.quit()
-            print('__FacebookLoginPage destroyed')
-
-        def _login(self, user, password):
-            if not self.logged:
-                self._email_field.send_keys(user)
-                self._pass_field.send_keys(password)
-                # self._login_button.click()
-                self._pass_field.submit()
-                self.logged = True
+    def _login(self, user, password):
+        print('Facebook login')
+        if not self.logged:
+            self._email_field.send_keys(user)
+            self._pass_field.send_keys(password)
+            # self._login_button.click()
+            self._pass_field.submit()
+            self.logged = True
 
 
-        def open_fanpage(self, url):
-            self._driver.get(url)
-            return self._driver
+    def open_fanpage(self, url):
+        print('Facebook open_fanpage')
+        self._driver.get(url)
+        return self._driver
+
+    def close(self):
+        print('Facebook close')
+        self._driver.quit()
+
+
 
 # import requests
 
--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -8,9 +8,11 @@ class TestFacebookLogin(unittest.TestCase):
 
     @classmethod
     def setUp(self):
+        print('TestFacebookLogin.setUp')
         self.user, self.password = get_credentials()
 
     def test_login(self):
+        print('TestFacebook.test_login')
         facebook = Facebook(self.user,self.password)
         # TODO Incluir o nome da pessoa no credentials.txt tambem, pra nao ficar fixo aqui.
 
@@ -23,20 +25,31 @@ class TestFacebookLogin(unittest.TestCase):
 
         self.assertEquals('Alexandre', username)
 
+
 class TestFacebook(unittest.TestCase):
 
     @classmethod
     def setUp(self):
+        print('TestFacebook.setUp')
+
         self.user, self.password = get_credentials()
         self.facebook = Facebook(self.user,self.password)
 
+    def tearDown(self):
+        print('TestFacebook.tearDown')
+        self.facebook.close()
 
     def test_open_fanpage(self):
+        print('TestFacebook.test_open_fanpage')
+
         fanpage = self.facebook.open_fanpage('https://www.facebook.com/jdoriajr')
 
         pagename = fanpage.find_element_by_xpath("//*[@id='pages_navigation']/div/div/div/div/div[3]/a").text
         self.assertEquals('@jdoriajr', pagename)
 
+    def test_posts(self):
+        # //*[@id="u_0_t"]/div[2]/div[1]
+        pass
 
 if __name__ == '__main__':
     unittest.main()
